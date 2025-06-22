174e5ab23c1ee3ebd5e79655a61488016a7d916f
Steve Corbett <steve@corbett.ca>
2025-06-21 23:43:30 -0600

    Tweaking template-launcher

--- src/templates/template-launcher.sh
+++ src/templates/template-launcher.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-# Shorthand wrapper script for invoking ApplicationGoesHere.
-
+# Invokes ApplicationGoesHere with default options.
 APPLICATION=ApplicationGoesHere
+
+# Figure out where the application configuration
+# and extensions should be stored. You can alter
+# these paths if you wish to store them elsewhere.
+USER_HOME=`readpath ~`
+APPLICATION_HOME="${USER_HOME}/.${APPLICATION}"
+EXTENSIONS_DIR="${APPLICATION_HOME}/extensions"
+JAVA_MEM="JavaMemGoesHere"
 
 # We need to find the directory where ApplicationGoesHere is installed,
 # and the name of the script that was invoked.
-# (We're assuming here that this wrapper script will always be named
-# after the application that it launches).
 # Note readlink -f is used to disambiguate things in case we were
 # invoked from a symlink  in /usr/bin or something.
-APPLICATION_HOME=`readlink -f $0`
-APPLICATION_HOME=`dirname $APPLICATION_HOME`
-APPLICATION_HOME=`dirname $APPLICATION_HOME`
+INSTALL_DIR=`readlink -f $0`
+INSTALL_DIR=`dirname $INSTALL_DIR`
+INSTALL_DIR=`dirname $INSTALL_DIR`
 # (two dirnames in a row - first gets the bin directory, second gets bin's parent,
 # which by convention should be the actual application home directory).
 
-if [ ! -f ${APPLICATION_HOME}/${APPLICATION}.jar ]; then
+if [ ! -f ${INSTALL_DIR}/${APPLICATION}.jar ]; then
   echo "${APPLICATION} does not seem to be installed."
   exit 1;
 fi
@@ -36,17 +41,8 @@ if [ "${JAVA}" == "" ]; then
   fi
 fi
 
-# TODO we could put Xmx options here if the application requires more memory...
-
-# TODO we need to set the extensions directory here also, because the application
-#      shouldn't assume it will always be under APPLICATION_HOME
-
-# TODO APPLICATION_HOME is actually wrong here. This is holdover code froom
-#      old sc-util. We used to distinguish between the directory where the application
-#      was installed (/opt/ApplicationGoesHere) and the user directory where
-#      mutable settings could be stored (typically ~/.ApplicationGoesHere).
-#      Since the migration from sc-util, applications basically don't care about
-#      APPLICATION_HOME anymore and only want the user home.
-
-# Invoke ApplicationGoesHere with our home property set:
-$JAVA -D${APPLICATION}_HOME=${APPLICATION_HOME} -jar ${APPLICATION_HOME}/${APPLICATION}.jar $*
+# Invoke ApplicationGoesHere with home and extensions dirs:
+$JAVA ${JAVA_MEM} \
+  -DAPPLICATION_HOME=${APPLICATION_HOME} \
+  -DEXTENSIONS_DIR=${EXTENSIONS_DIR} \
+  -jar ${INSTALL_DIR}/${APPLICATION}.jar $*
